38490114ff661981fdc4af010d884989bc8f9d4e
YashiDixitt <147697574+YashiDixitt@users.noreply.github.com>
2023-10-12 13:27:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let a=document.getElementById("btn").addEventListener("click",mass);
+
+
+
+function mass(){
+    alert("Hey baby I LOVE YOU ❤  I MISS YOU SO MUCH ");
+    setTimeout(()=>{
+        alert("HAPPY BIRTHDAY MY DARLING ❤");
+    },3000)
+}
